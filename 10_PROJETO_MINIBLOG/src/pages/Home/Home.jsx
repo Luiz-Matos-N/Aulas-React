2c274a47f8f776cdfb1adcf019b0dc07a80839c5
Luiz Fernandes <luiz.fernandes31matos@gmail.com>
2024-06-20 18:43:52 -0300

    Criação da página Home e do mecanismo de busca

--- 10_PROJETO_MINIBLOG/src/pages/Home/Home.jsx
+++ 10_PROJETO_MINIBLOG/src/pages/Home/Home.jsx
@@ -4,15 +4,23 @@ import styles from "./Home.module.css";
 //hooks
 import { useNavigate, Link } from "react-router-dom";
 import { useState } from "react";
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 
 //components
+import PostDetails from "../../components/PostDetails";
 
 const Home = () => {
   const [query, setQuery] = useState("");
-  const [posts] = useState([]);
+  const { documents: posts, loading } = useFetchDocuments("posts");
+
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (query) {
+      return navigate(`/search?q=${query}`);
+    }
   };
 
   return (
@@ -27,7 +35,9 @@ const Home = () => {
         <button className="btn btn-dark">Pesquisar</button>
       </form>
       <div>
-        <h1>Posts...</h1>
+        {loading && <p>Carregando...</p>}
+        {posts &&
+          posts.map((post) => <PostDetails key={post.id} post={post} />)}
         {posts && posts.length === 0 && (
           <div className={styles.noposts}>
             <p>Nenhum post foi encontrado...</p>
