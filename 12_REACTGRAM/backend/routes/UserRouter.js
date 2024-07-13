4fa7a160b0aa70b9f673cab25ffc10b6f5dd5357
Luiz Fernandes <luiz.fernandes31matos@gmail.com>
2024-07-13 15:47:03 -0300

    Criação dos middlewares, controllers e models

---
+++ 12_REACTGRAM/backend/routes/UserRouter.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const router = express.Router();
+
+//Controller
+const { register } = require("../controllers/UserController");
+
+//Middlewares
+const validate = require("../middlewares/handleValidation");
+const { userCreateValidation } = require("../middlewares/userValidation");
+
+//Routes
+
+router.post("/register", userCreateValidation(), validate, register);
+
+module.exports = router;
