474704f04682559314c35e61880d28654dffe959
Luiz Fernandes <luiz.fernandes31matos@gmail.com>
2024-07-26 18:58:22 -0300

    Criação da rota de login

--- 12_REACTGRAM/backend/middlewares/userValidation.js
+++ 12_REACTGRAM/backend/middlewares/userValidation.js
@@ -29,6 +29,18 @@ const userCreateValidation = () => {
   ];
 };
 
+const loginValidation = () => {
+  return [
+    body("email")
+      .isString()
+      .withMessage("O email é obrigatório.")
+      .isEmail()
+      .withMessage("Insira um email válido."),
+    body("password").isString().withMessage("A sennha é obrigatória"),
+  ];
+};
+
 module.exports = {
   userCreateValidation,
+  loginValidation,
 };
