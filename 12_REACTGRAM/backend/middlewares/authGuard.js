6f8ad8f595206383395910ebd502e2db94a1218d
Luiz Fernandes <luiz.fernandes31matos@gmail.com>
2024-08-01 18:35:53 -0300

    criação do middleware de imageUpload

--- 12_REACTGRAM/backend/middlewares/authGuard.js
+++ 12_REACTGRAM/backend/middlewares/authGuard.js
@@ -1,4 +1,4 @@
-const user = requira("../models/User");
+const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 
 const jwtSecret = process.env.JWT_SECRET;
@@ -8,13 +8,14 @@ const authGuard = async (req, res, next) => {
   const token = authHeader && authHeader.split(" ")[1];
 
   //check if header has a token
-  if (!token) return res.status(401).json({ errors: ["Acesso negado!"] });
+  if (!token) {
+    return res.status(401).json({ errors: ["Acesso negado!"] });
+  }
 
   //check if token is valid
   try {
     const verified = jwt.verify(token, jwtSecret);
-    req.user = await User.findById(verified.id).select;
-    ("-password");
+    req.user = await User.findById(verified.id).select("-password");
     next();
   } catch (error) {
     res.status(401).json({ error: ["Token inválido."] });
